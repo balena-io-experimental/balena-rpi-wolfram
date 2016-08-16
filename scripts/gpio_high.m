5e4dcdc62f32a983ccd652fd376becdc49e943d3
Gergely Imreh <imrehg@gmail.com>
2016-08-16 21:01:56 +0800

    add more info to GPIO scripts

--- scripts/gpio_high.m
+++ scripts/gpio_high.m
@@ -1,2 +1,3 @@
-Print["Setting GPIO 24 to High"]
+Print["Setting GPIO 24 to High: start"]
 DeviceWrite["GPIO", 24 -> 1]
+Print["Setting GPIO 24 to High: done"]
--- scripts/gpio_low.m
+++ scripts/gpio_low.m
@@ -1,2 +1,3 @@
-Print["Setting GPIO 24 to Low"]
+Print["Setting GPIO 24 to Low: start"]
 DeviceWrite["GPIO", 24 -> 0]
+Print["Setting GPIO 24 to Low: done"]
