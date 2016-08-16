e5478723e49bef4e7bf8719f5f60fdeef7a44e13
Gergely Imreh <imrehg@gmail.com>
2016-08-16 18:59:44 +0800

    update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 
+# Set up I2C
 modprobe i2c-dev
+
+# /boot/config.txt is not accessible from the application, as it only lives in the host OS
+# still need to create this file to make Mathematica happy
+touch /boot/config.txt
 
 while : ; do
   echo "Idling..."
